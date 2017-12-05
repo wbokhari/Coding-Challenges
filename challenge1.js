1067d89d7f255329b9ee1a7255eca6daa217d120
Wasif Bokhari <wasifbokhari@gmail.com>
2017-12-04 22:38:15 -0700

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -2,21 +2,18 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* It's very easy to deploy app using Heroku and Mlab. The tutorial walks through how to use express.js, node.js and set up and deploy
+heroku CLI.    */
 
 router.post('/messages', function(req, res, next) {
   var hash = crypto.createHash('sha256').update(req.body.message).digest('hex');
-  console.log(hash);
+  // Save the hash and word in the database as key value pairs
   res.json(hash);
 });
 
 router.get('/messages', function(req, res, next) {
-  //var hash = crypto.createHash('sha256').update(req.body.message).digest('hex');
-  console.log(req.path);
-  //res.json(hash);
+  // this will just query the mongo db using hash as key and return the value or 404 if it does not exist in our db
+  res.json(req.path);
 });
 
 module.exports = router;
